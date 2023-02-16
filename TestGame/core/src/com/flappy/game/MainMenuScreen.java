bae4d0573b79437b114a558f1c93b24df74409e5
antonwilen <antwil1107@skola.goteborg.se>
2023-02-16 11:56:36 +0100

    added todo comment

--- TestGame/core/src/com/flappy/game/MainMenuScreen.java
+++ TestGame/core/src/com/flappy/game/MainMenuScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainMenuScreen implements Screen {
+    // TODO make a timer class
     private final int SCREEN_HEIGHT = 480;
     private final int SCREEN_WIDTH = 800;
     final Flap game;
