b84e1187cede37c842967452e61dc856e4bd27f3
stuntmannen <117079747+danjoh2019@users.noreply.github.com>
2023-04-04 14:25:58 +0200

    added snow images

--- core/src/com/flappy/game/objects/Snow.java
+++ core/src/com/flappy/game/objects/Snow.java
@@ -1,9 +1,23 @@
 package com.flappy.game.objects;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Snow {
     public Image snowflake;
+
+    public Snow() {
+        List<Image> snowflakes = new ArrayList<>();
+        snowflakes.add(new Image(new Texture("gfx/snow/snow1.png")));
+        snowflakes.add(new Image(new Texture("gfx/snow/snow2.png")));
+        snowflakes.add(new Image(new Texture("gfx/snow/snow3.png")));
+
+        this.snowflake = snowflakes.get(MathUtils.random(2));
+    }
 
     public Image getSnowflake() {
         return snowflake;
