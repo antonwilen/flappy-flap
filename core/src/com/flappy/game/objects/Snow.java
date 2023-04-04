8f6bc7107d94e83b117235b08efc67d8f33e2f6a
stuntmannen <117079747+danjoh2019@users.noreply.github.com>
2023-04-04 14:13:20 +0200

    added snow

---
+++ core/src/com/flappy/game/objects/Snow.java
@@ -0,0 +1,11 @@
+package com.flappy.game.objects;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Snow {
+    public Image snowflake;
+
+    public Image getSnowflake() {
+        return snowflake;
+    }
+}
